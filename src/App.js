4a3248821c3cfd0d07eb8826b20918815a69a285
binyoma <innocentbinyoma@pm.me>
2022-10-02 11:45:53 +0200

    alert

--- src/App.js
+++ src/App.js
@@ -3,35 +3,54 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  const [name, setName]=useState('');
-  const [list, setList]=useState([]);
-  const [isEditing, setIsEditing]=useState(false);
-  const [editID, setEditID]=useState(null);
-  const [alert, setAlert]=useState({show:false, mesg:'',type:''});
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log('hello');
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: true, msg: 'hello', type: 'danger' })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      // display alert
+      showAlert( true, 'danger', 'please enter value');
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      //show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('')
+    }
   }
-  return <section className='section-center'>
-    <form className='grocery-form' onSubmit={handleSubmit}>
-      {alert.show && <Alert/>}
-      <h3>grocery bud</h3>
-      <div className='form-control'>
-        <input 
-         type='text'
-         className='grocery'
-          placeholder='e. g. eggs'
-          value={name}
-          onChange={(e)=>setName(e.target.value)}/>
-        <button type='submit' className='submit-btn'>{isEditing? 'edit':'submit'}</button>
-      </div>
-           
-    </form>
-    <div className='grocery-container'>
-      <List/>
-      <button className='clear-btn'>clear items</button>
-    </div>
-  </section>
+  const showAlert=(show=false, type="",msg="")=>{
+     setAlert({show, type, msg});
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e. g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default App
