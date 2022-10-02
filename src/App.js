71befef2591771b4adbb23c8f825912e5a7b8b38
binyoma <innocentbinyoma@pm.me>
2022-10-02 13:27:31 +0200

    remove and edit item

--- src/App.js
+++ src/App.js
@@ -15,11 +15,21 @@ function App() {
       showAlert( true, 'danger', 'please enter value');
     } else if (name && isEditing) {
       // deal with edit
+      setList(list.map((item)=>{
+        if (item.id ===editID) {
+          return {...item, title:name}
+        } return item
+      }));
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
     } else {
       showAlert(true, 'success','item added to the list');
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName('');
+      
     }
   }
   const showAlert=(show=false, type="",msg="")=>{
@@ -29,10 +39,20 @@ function App() {
     showAlert(true, 'danger','empty list');
     setList([]);
   }
+  const removeItem=(id)=>{
+    showAlert(true, 'danger','item removed');
+    setList(list.filter((item)=>item.id !==id))
+  }
+  const editItem=(id)=>{
+    const specificItem=list.find((item)=>item.id ===id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title)
+  }
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -49,7 +69,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list}  removeItem={removeItem} editItem={editItem}/>
           <button className="clear-btn" onClick={clearList}>clear items</button>
         </div>
       )}
