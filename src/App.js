2ada4399a1892f9c726965c2d68a9c6879553c37
binyoma <innocentbinyoma@pm.me>
2022-10-02 09:43:55 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [name, setName]=useState('');
+  const [list, setList]=useState([]);
+  const [isEditing, setIsEditing]=useState(false);
+  const [editID, setEditID]=useState(null);
+  const [alert, setAlert]=useState({show:false, mesg:'',type:''});
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log('hello');
+  }
+  return <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert/>}
+      <h3>grocery bud</h3>
+      <div className='form-control'>
+        <input 
+         type='text'
+         className='grocery'
+          placeholder='e. g. eggs'
+          value={name}
+          onChange={(e)=>setName(e.target.value)}/>
+        <button type='submit' className='submit-btn'>{isEditing? 'edit':'submit'}</button>
+      </div>
+           
+    </form>
+    <div className='grocery-container'>
+      <List/>
+      <button className='clear-btn'>clear items</button>
     </div>
-  );
+  </section>
 }
 
-export default App;
+export default App
